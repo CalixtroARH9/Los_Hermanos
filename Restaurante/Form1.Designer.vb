a68f7d31d2cb8414c2bfa4e74770262f17f4e87e
Alejandro <al052178@cncivirtual.mx>
2022-02-20 15:03:42 -0600

    Sistema
    Sistema BASE

---
+++ Restaurante/Form1.Designer.vb
@@ -0,0 +1,340 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Pedidos
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox6 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox8 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox7 = New System.Windows.Forms.GroupBox()
+        Me.lbAlimentos = New System.Windows.Forms.ListBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lbPedido = New System.Windows.Forms.ListBox()
+        Me.lblt = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lbBebidas = New System.Windows.Forms.ListBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox6.SuspendLayout()
+        Me.GroupBox5.SuspendLayout()
+        Me.GroupBox8.SuspendLayout()
+        Me.GroupBox7.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.GroupBox6)
+        Me.GroupBox1.Controls.Add(Me.GroupBox5)
+        Me.GroupBox1.Controls.Add(Me.GroupBox4)
+        Me.GroupBox1.Controls.Add(Me.GroupBox3)
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Location = New System.Drawing.Point(9, 32)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(558, 245)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Menú de pedidos"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(367, 216)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(184, 23)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Enviar a cocina"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox6
+        '
+        Me.GroupBox6.Controls.Add(Me.RadioButton3)
+        Me.GroupBox6.Controls.Add(Me.RadioButton2)
+        Me.GroupBox6.Controls.Add(Me.RadioButton1)
+        Me.GroupBox6.Location = New System.Drawing.Point(342, 19)
+        Me.GroupBox6.Name = "GroupBox6"
+        Me.GroupBox6.Size = New System.Drawing.Size(208, 49)
+        Me.GroupBox6.TabIndex = 11
+        Me.GroupBox6.TabStop = False
+        Me.GroupBox6.Text = "Turno"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(145, 21)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(57, 17)
+        Me.RadioButton3.TabIndex = 3
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Noche"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(85, 22)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(53, 17)
+        Me.RadioButton2.TabIndex = 2
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Tarde"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(15, 22)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(64, 17)
+        Me.RadioButton1.TabIndex = 1
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Mañana"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Controls.Add(Me.GroupBox8)
+        Me.GroupBox5.Controls.Add(Me.GroupBox7)
+        Me.GroupBox5.Location = New System.Drawing.Point(6, 74)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(355, 165)
+        Me.GroupBox5.TabIndex = 10
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "Menú"
+        '
+        'GroupBox8
+        '
+        Me.GroupBox8.Controls.Add(Me.lbBebidas)
+        Me.GroupBox8.Location = New System.Drawing.Point(179, 19)
+        Me.GroupBox8.Name = "GroupBox8"
+        Me.GroupBox8.Size = New System.Drawing.Size(167, 140)
+        Me.GroupBox8.TabIndex = 2
+        Me.GroupBox8.TabStop = False
+        Me.GroupBox8.Text = "Bebidas"
+        '
+        'GroupBox7
+        '
+        Me.GroupBox7.Controls.Add(Me.lbAlimentos)
+        Me.GroupBox7.Location = New System.Drawing.Point(6, 19)
+        Me.GroupBox7.Name = "GroupBox7"
+        Me.GroupBox7.Size = New System.Drawing.Size(167, 140)
+        Me.GroupBox7.TabIndex = 1
+        Me.GroupBox7.TabStop = False
+        Me.GroupBox7.Text = "Alimentos"
+        '
+        'lbAlimentos
+        '
+        Me.lbAlimentos.FormattingEnabled = True
+        Me.lbAlimentos.Items.AddRange(New Object() {"Dona de chocolate $15", "Dona de vainilla $15", "Panini $25", "Brownies $30", "Galletas de chocolate $10"})
+        Me.lbAlimentos.Location = New System.Drawing.Point(6, 19)
+        Me.lbAlimentos.Name = "lbAlimentos"
+        Me.lbAlimentos.Size = New System.Drawing.Size(155, 108)
+        Me.lbAlimentos.TabIndex = 11
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Label1)
+        Me.GroupBox4.Controls.Add(Me.ComboBox1)
+        Me.GroupBox4.Location = New System.Drawing.Point(161, 19)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(175, 49)
+        Me.GroupBox4.TabIndex = 9
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Mesero que atiende"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Código"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010"})
+        Me.ComboBox1.Location = New System.Drawing.Point(48, 22)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.ComboBox2)
+        Me.GroupBox3.Location = New System.Drawing.Point(6, 19)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(149, 49)
+        Me.GroupBox3.TabIndex = 7
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Número de mesa"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"0001", "0002", "0003", "0004"})
+        Me.ComboBox2.Location = New System.Drawing.Point(6, 22)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(137, 21)
+        Me.ComboBox2.TabIndex = 3
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lbPedido)
+        Me.GroupBox2.Controls.Add(Me.lblt)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(367, 74)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(184, 136)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Pedido del cliente"
+        '
+        'lbPedido
+        '
+        Me.lbPedido.FormattingEnabled = True
+        Me.lbPedido.Location = New System.Drawing.Point(6, 19)
+        Me.lbPedido.Name = "lbPedido"
+        Me.lbPedido.Size = New System.Drawing.Size(171, 95)
+        Me.lbPedido.TabIndex = 10
+        '
+        'lblt
+        '
+        Me.lblt.AutoSize = True
+        Me.lblt.Location = New System.Drawing.Point(74, 120)
+        Me.lblt.Name = "lblt"
+        Me.lblt.Size = New System.Drawing.Size(103, 13)
+        Me.lblt.TabIndex = 9
+        Me.lblt.Text = "000,000,000,000.00"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(55, 120)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "$"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(1, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Total:"
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(492, 3)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 3
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(219, 8)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(132, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Restaurante los Hermanos"
+        '
+        'lbBebidas
+        '
+        Me.lbBebidas.FormattingEnabled = True
+        Me.lbBebidas.Items.AddRange(New Object() {"Refresco coca cola $15", "Refresco Pepsi $15", "Café americano $15", "Café expreso $15", "Agua $20"})
+        Me.lbBebidas.Location = New System.Drawing.Point(6, 19)
+        Me.lbBebidas.Name = "lbBebidas"
+        Me.lbBebidas.Size = New System.Drawing.Size(155, 108)
+        Me.lbBebidas.TabIndex = 1
+        '
+        'Pedidos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(571, 283)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnSalir)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Pedidos"
+        Me.Text = "Restaurante ""Los Hermanos"""
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox6.ResumeLayout(False)
+        Me.GroupBox6.PerformLayout()
+        Me.GroupBox5.ResumeLayout(False)
+        Me.GroupBox8.ResumeLayout(False)
+        Me.GroupBox7.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox6 As GroupBox
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents GroupBox8 As GroupBox
+    Friend WithEvents GroupBox7 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblt As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lbPedido As ListBox
+    Friend WithEvents lbAlimentos As ListBox
+    Friend WithEvents lbBebidas As ListBox
+End Class
---
+++ Restaurante/Form1.vb
@@ -0,0 +1,64 @@
+﻿Public Class Pedidos
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+    Dim total As Double
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Close()
+    End Sub
+    Private Sub lbAlimentos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbAlimentos.SelectedIndexChanged
+        Select Case lbAlimentos.SelectedIndex
+            Case 0
+                lbPedido.Items.Add("Dona de chocolate $15")
+                total = total + 15
+                lblt.Text = total
+            Case 1
+                lbPedido.Items.Add("Dona de vainilla $15")
+                total = total + 15
+                lblt.Text = total
+            Case 2
+                lbPedido.Items.Add("Panini $25")
+                total = total + 25
+                lblt.Text = total
+            Case 3
+                lbPedido.Items.Add("Brownies $30")
+                total = total + 30
+                lblt.Text = total
+            Case 4
+                lbPedido.Items.Add("Galletas de chocolate $10")
+                total = total + 10
+                lblt.Text = total
+        End Select
+    End Sub
+
+    Private Sub lbBebidas_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbBebidas.SelectedIndexChanged
+        Select Case lbAlimentos.SelectedIndex
+
+            Case 0
+                lbPedido.Items.Add("Refresco coca cola $15")
+                total = total + 15
+                lblt.Text = total
+            Case 1
+                lbPedido.Items.Add("Refresco Pepsi $15")
+                total = total + 15
+                lblt.Text = total
+            Case 2
+                lbPedido.Items.Add("Café americano $15")
+                total = total + 15
+                lblt.Text = total
+            Case 3
+                lbPedido.Items.Add("Café expreso $15")
+                total = total + 15
+                lblt.Text = total
+            Case 4
+                lbPedido.Items.Add("Agua $20")
+                total = total + 20
+                lblt.Text = total
+
+        End Select
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show(total, "El total a pagar es: ")
+
+    End Sub
+End Class
